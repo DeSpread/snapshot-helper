b9e61e4c276a76df9e27f300940a4614d1bbeb93
Hyeongjun Yu <hyeongjun.dev@gmail.com>
2024-09-28 00:33:23 +0900

    feature: change to async return in SnapshotController

--- src/main/kotlin/com/despread/snapshothelper/controller/SnapshotController.kt
+++ src/main/kotlin/com/despread/snapshothelper/controller/SnapshotController.kt
@@ -2,24 +2,27 @@ package com.despread.snapshothelper.controller
 
 import com.despread.snapshothelper.model.SnapshotDto
 import com.despread.snapshothelper.service.AptosSnapshotService
-import org.springframework.http.HttpStatus
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.coroutines.CoroutineContext
 
 @RestController
 @RequestMapping("/api/v1")
-class SnapshotController(private val aptosSnapshotService: AptosSnapshotService) {
+class SnapshotController(private val aptosSnapshotService: AptosSnapshotService) : CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO
 
-    @PostMapping("/snapshot")
-    suspend fun snapshot(@RequestBody snapshotDto: SnapshotDto): ResponseEntity<Void> {
-        return try {
+    @PostMapping("/snapshot/multipart")
+    suspend fun snapshot(@RequestBody snapshotDto: SnapshotDto): ResponseEntity<String> {
+        launch {
             aptosSnapshotService.snapshot(snapshotDto)
-            ResponseEntity(HttpStatus.CREATED)
-        } catch (e: Exception) {
-            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
         }
+        return ResponseEntity.accepted().body("File uploaded successfully")
     }
 }
